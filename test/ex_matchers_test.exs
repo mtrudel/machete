e7ab6ea4c4bacead15df1c3365a4b73791a8762f
Mat Trudel <mat@geeky.net>
2022-06-06 10:22:42 -0400

    Add top-level ex_matchers test to validate operators

---
+++ test/ex_matchers_test.exs
@@ -0,0 +1,16 @@
+defmodule ExMatchersTest do
+  use ExUnit.Case, async: true
+  use ExMatchers
+
+  test "the ~> operator is defined and returns success" do
+    assert 1 ~> 1
+  end
+
+  test "the ~> operator is defined and returns failure" do
+    refute 1 ~> 0
+  end
+
+  test "the ~>> operator is defined and returns mismatches" do
+    assert 1 ~>> 0 == [%ExMatchers.Mismatch{message: "Literals do not match", path: []}]
+  end
+end
