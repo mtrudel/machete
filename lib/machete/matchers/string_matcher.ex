efed119846523ace384a09b97ba048b67e4f3c33
Mat Trudel <mat@geeky.net>
2022-06-19 21:00:20 -0400

    Add a bunch of string matcher parameters

--- lib/machete/matchers/string_matcher.ex
+++ lib/machete/matchers/string_matcher.ex
@@ -3,7 +3,17 @@ defmodule Machete.StringMatcher do
 
   import Machete.Mismatch
 
-  defstruct empty: nil, length: nil, min: nil, max: nil
+  defstruct empty: nil,
+            length: nil,
+            min: nil,
+            max: nil,
+            matches: nil,
+            alphabetic: nil,
+            lowercase: nil,
+            uppercase: nil,
+            alphanumeric: nil,
+            numeric: nil,
+            hexadecimal: nil
 
   def string(opts \\ []), do: struct!(__MODULE__, opts)
 
@@ -13,7 +23,14 @@ defmodule Machete.StringMatcher do
            nil <- matches_empty(b, a.empty),
            nil <- matches_length(b, a.length),
            nil <- matches_min(b, a.min),
-           nil <- matches_max(b, a.max) do
+           nil <- matches_max(b, a.max),
+           nil <- matches_regex(b, a.matches),
+           nil <- matches_alphabetic(b, a.alphabetic),
+           nil <- matches_lowercase(b, a.lowercase),
+           nil <- matches_uppercase(b, a.uppercase),
+           nil <- matches_alphanumeric(b, a.alphanumeric),
+           nil <- matches_numeric(b, a.numeric),
+           nil <- matches_hexadecimal(b, a.hexadecimal) do
       end
     end
 
@@ -45,6 +62,39 @@ defmodule Machete.StringMatcher do
     defp matches_max(b, length) do
       unless String.length(b) <= length do
         mismatch("#{inspect(b)} is more than #{length} characters")
+      end
+    end
+
+    defp matches_regex(_, nil), do: nil
+
+    defp matches_regex(b, regex) do
+      unless b =~ regex do
+        mismatch("#{inspect(b)} does not match #{inspect(regex)}")
+      end
+    end
+
+    for {name, regex} <- [
+          {"alphabetic", "^[[:alpha:]]+$"},
+          {"lowercase", "^[[:lower:]]+$"},
+          {"uppercase", "^[[:upper:]]+$"},
+          {"alphanumeric", "^[[:alnum:]]+$"},
+          {"numeric", "^[[:digit:]]+$"},
+          {"hexadecimal", "^[[:xdigit:]]+$"}
+        ] do
+      fn_name = String.to_atom("matches_#{name}")
+
+      def unquote(fn_name)(_, nil), do: nil
+
+      def unquote(fn_name)(b, false) do
+        if b =~ Regex.compile!(unquote(regex)) do
+          mismatch("#{inspect(b)} is #{unquote(name)}")
+        end
+      end
+
+      def unquote(fn_name)(b, true) do
+        unless b =~ Regex.compile!(unquote(regex)) do
+          mismatch("#{inspect(b)} is not #{unquote(name)}")
+        end
       end
     end
   end
--- test/machete/matchers/string_matcher_test.exs
+++ test/machete/matchers/string_matcher_test.exs
@@ -28,6 +28,58 @@ defmodule StringMatcherTest do
     assert "abc" ~> string(empty: false)
   end
 
+  test "matches strings on match" do
+    assert "abc" ~> string(matches: ~r/abc/)
+  end
+
+  test "matches strings on alphabetic true" do
+    assert "abc" ~> string(alphabetic: true)
+  end
+
+  test "matches strings on alphabetic false" do
+    assert "123" ~> string(alphabetic: false)
+  end
+
+  test "matches strings on lowercase true" do
+    assert "abc" ~> string(lowercase: true)
+  end
+
+  test "matches strings on lowercase false" do
+    assert "ABC" ~> string(lowercase: false)
+  end
+
+  test "matches strings on uppercase true" do
+    assert "ABC" ~> string(uppercase: true)
+  end
+
+  test "matches strings on uppercase false" do
+    assert "abc" ~> string(uppercase: false)
+  end
+
+  test "matches strings on alphanumeric true" do
+    assert "abc123" ~> string(alphanumeric: true)
+  end
+
+  test "matches strings on alphanumeric false" do
+    assert "$" ~> string(alphanumeric: false)
+  end
+
+  test "matches strings on numeric true" do
+    assert "123" ~> string(numeric: true)
+  end
+
+  test "matches strings on numeric false" do
+    assert "abc" ~> string(numeric: false)
+  end
+
+  test "matches strings on hexadecimal true" do
+    assert "deadbeef0123" ~> string(hexadecimal: true)
+  end
+
+  test "matches strings on hexadecimal false" do
+    assert "ghi" ~> string(hexadecimal: false)
+  end
+
   test "produces a useful mismatch for non strings" do
     assert 1 ~>> string() ~> mismatch("1 is not a string")
   end
@@ -50,5 +102,59 @@ defmodule StringMatcherTest do
 
   test "produces a useful mismatch for empty false" do
     assert "" ~>> string(empty: false) ~> mismatch("\"\" is empty")
+  end
+
+  test "produces a useful mismatch for match" do
+    assert "def" ~>> string(matches: ~r/abc/) ~> mismatch("\"def\" does not match ~r/abc/")
+  end
+
+  test "produces a useful mismatch for alphabetic true" do
+    assert "123" ~>> string(alphabetic: true) ~> mismatch("\"123\" is not alphabetic")
+  end
+
+  test "produces a useful mismatch for alphabetic false" do
+    assert "abc" ~>> string(alphabetic: false) ~> mismatch("\"abc\" is alphabetic")
+  end
+
+  test "produces a useful mismatch for uppercase true" do
+    assert "abc" ~>> string(uppercase: true) ~> mismatch("\"abc\" is not uppercase")
+  end
+
+  test "produces a useful mismatch for uppercase false" do
+    assert "ABC" ~>> string(uppercase: false) ~> mismatch("\"ABC\" is uppercase")
+  end
+
+  test "produces a useful mismatch for lowercase true" do
+    assert "ABC" ~>> string(lowercase: true) ~> mismatch("\"ABC\" is not lowercase")
+  end
+
+  test "produces a useful mismatch for lowercase false" do
+    assert "abc" ~>> string(lowercase: false) ~> mismatch("\"abc\" is lowercase")
+  end
+
+  test "produces a useful mismatch for alphanumeric true" do
+    assert "$" ~>> string(alphanumeric: true) ~> mismatch("\"$\" is not alphanumeric")
+  end
+
+  test "produces a useful mismatch for alphanumeric false" do
+    assert "abc123" ~>> string(alphanumeric: false) ~> mismatch("\"abc123\" is alphanumeric")
+  end
+
+  test "produces a useful mismatch for numeric true" do
+    assert "abc" ~>> string(numeric: true) ~> mismatch("\"abc\" is not numeric")
+  end
+
+  test "produces a useful mismatch for numeric false" do
+    assert "123" ~>> string(numeric: false) ~> mismatch("\"123\" is numeric")
+  end
+
+  test "produces a useful mismatch for hexadecimal true" do
+    assert "ghi" ~>> string(hexadecimal: true) ~> mismatch("\"ghi\" is not hexadecimal")
+  end
+
+  test "produces a useful mismatch for hexadecimal false" do
+    assert "deadbeef0123"
+           ~>> string(hexadecimal: false)
+           ~> mismatch("\"deadbeef0123\" is hexadecimal")
   end
 end
