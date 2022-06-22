8b07307ffafb7d53bc2570ecfbcd59ef8d295dc6
Mat Trudel <mat@geeky.net>
2022-06-22 09:00:57 -0400

    Add keys, values, size, min, max attributes to map matcher

--- lib/machete/matchers/map_matcher.ex
+++ lib/machete/matchers/map_matcher.ex
@@ -5,7 +5,7 @@ defmodule Machete.MapMatcher do
 
   import Machete.Mismatch
 
-  defstruct []
+  defstruct keys: nil, values: nil, size: nil, min: nil, max: nil
 
   @typedoc """
   Describes an instance of this matcher
@@ -15,23 +15,97 @@ defmodule Machete.MapMatcher do
   @typedoc """
   Describes the arguments that can be passed to this matcher
   """
-  @type opts :: []
+  @type opts :: [
+          {:keys, Machete.Matchable.t()},
+          {:values, Machete.Matchable.t()},
+          {:size, non_neg_integer()},
+          {:min, non_neg_integer()},
+          {:max, non_neg_integer()}
+        ]
 
   @doc """
-  Matches against map values
+  Matches maps. Useful for cases where you wish to match against the general shape / size of
+  a map, but cannot match against a literal map (or use the `Machete.Superset` / `Machete.Subset`
+  matchers)
 
-  Takes no arguments
+  Takes the following arguments:
+
+  * `keys`: A matcher to use against all keys in the map
+  * `values`: A matcher to use against all values in the map
+  * `size`: Requires the matched map to be exactly the specified size
+  * `min`: Requires the matched map to be greater than or equal to the specified size
+  * `max`: Requires the matched map to be less than or equal to the specified size
 
   Examples:
 
-      iex> assert %{} ~> map()
+      iex> assert %{a: 1} ~> map()
+      true
+
+      iex> assert %{a: 1} ~> map(keys: atom())
+      true
+
+      iex> assert %{a: 1} ~> map(values: integer())
+      true
+
+      iex> assert %{a: 1} ~> map(size: 1)
+      true
+
+      iex> assert %{a: 1} ~> map(min: 1)
+      true
+
+      iex> assert %{a: 1} ~> map(max: 2)
       true
   """
   @spec map(opts()) :: t()
   def map(opts \\ []), do: struct!(__MODULE__, opts)
 
   defimpl Machete.Matchable do
-    def mismatches(%@for{}, b) when is_map(b), do: nil
-    def mismatches(%@for{}, b), do: mismatch("#{inspect(b)} is not a map")
+    def mismatches(%@for{}, b) when not is_map(b), do: mismatch("#{inspect(b)} is not a map")
+
+    def mismatches(%@for{} = a, b) do
+      with nil <- matches_size(b, a.size),
+           nil <- matches_min(b, a.min),
+           nil <- matches_max(b, a.max),
+           nil <- matches_keys(b, a.keys),
+           nil <- matches_values(b, a.values) do
+      end
+    end
+
+    defp matches_size(b, size) when is_number(size) and map_size(b) != size,
+      do: mismatch("#{inspect(b)} is not exactly #{size} pairs in size")
+
+    defp matches_size(_, _), do: nil
+
+    defp matches_min(b, min) when is_number(min) and map_size(b) < min,
+      do: mismatch("#{inspect(b)} is less than #{min} pairs in size")
+
+    defp matches_min(_, _), do: nil
+
+    defp matches_max(b, max) when is_number(max) and map_size(b) > max,
+      do: mismatch("#{inspect(b)} is greater than #{max} pairs in size")
+
+    defp matches_max(_, _), do: nil
+
+    defp matches_keys(_, nil), do: nil
+
+    defp matches_keys(b, matcher) do
+      b
+      |> Map.keys()
+      |> Enum.flat_map(fn k ->
+        (Machete.Matchable.mismatches(matcher, k) || [])
+        |> Enum.map(&%{&1 | path: [k | &1.path]})
+      end)
+    end
+
+    defp matches_values(_, nil), do: nil
+
+    defp matches_values(b, matcher) do
+      b
+      |> Map.keys()
+      |> Enum.flat_map(fn k ->
+        (Machete.Matchable.mismatches(matcher, b[k]) || [])
+        |> Enum.map(&%{&1 | path: [k | &1.path]})
+      end)
+    end
   end
 end
--- test/machete/matchers/map_matcher_test.exs
+++ test/machete/matchers/map_matcher_test.exs
@@ -9,4 +9,26 @@ defmodule MapMatcherTest do
   test "produces a useful mismatch for map mismatches" do
     assert 123 ~>> map() ~> mismatch("123 is not a map")
   end
+
+  test "produces a useful mismatch for key mismatches" do
+    assert %{a: 1} ~>> map(keys: integer()) ~> mismatch(":a is not an integer", :a)
+  end
+
+  test "produces a useful mismatch for value mismatches" do
+    assert %{a: 1} ~>> map(values: string()) ~> mismatch("1 is not a string", :a)
+  end
+
+  test "produces a useful mismatch for size mismatches" do
+    assert %{a: 1} ~>> map(size: 2) ~> mismatch("%{a: 1} is not exactly 2 pairs in size")
+  end
+
+  test "produces a useful mismatch for min size mismatches" do
+    assert %{a: 1} ~>> map(min: 2) ~> mismatch("%{a: 1} is less than 2 pairs in size")
+  end
+
+  test "produces a useful mismatch for max size mismatches" do
+    assert %{a: 1, b: 1}
+           ~>> map(max: 1)
+           ~> mismatch("%{a: 1, b: 1} is greater than 1 pairs in size")
+  end
 end
