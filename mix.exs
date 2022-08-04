d594f327d2de73ee885c4d30a28ce4d757e03b37
Mat Trudel <mat@geeky.net>
2022-08-04 08:18:43 -0400

    Version bump to 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Machete.MixProject do
   def project do
     [
       app: :machete,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
