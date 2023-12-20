2ad941e55847bb3184d7a3b89d3143d87b49cab7
Mat Trudel <mat@geeky.net>
2023-12-20 10:53:17 -0500

    Version bump to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Machete.MixProject do
   def project do
     [
       app: :machete,
-      version: "0.2.8",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
